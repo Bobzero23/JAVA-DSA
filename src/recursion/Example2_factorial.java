33e68f40a6f5643201b5c663678e0b145b786543
Bobzero <rajabu.mustafa.00@gmail.com>
2022-08-24 06:49:38 +0300

    DONE FOR NOW

--- src/recursion/Example2_factorial.java
+++ src/recursion/Example2_factorial.java
@@ -7,17 +7,21 @@ public class Example2_factorial {
 
     /*Recursion method*/
     static int factorial(int n){
+        /*base case*/
         if(n < 1){
             return 1;
         }
-
+        /*recursive case*/
         return n * factorial(n - 1);
     }
 
     /*main method*/
     public static void main(String[] args) {
 
-        /*calling the recursion method*/
-        factorial(5);
+        /*initializing the value of the method to a variable*/
+        var rec = factorial(5);
+
+        /*displaying the variable*/
+        System.out.println(rec);
     }
 }
