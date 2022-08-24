ef3d6d75a0ab24fa65dd54daf9127bf6f4d235df
Bobzero <rajabu.mustafa.00@gmail.com>
2022-08-24 15:38:06 +0300

    FIBONACCI

---
+++ src/recursion/Exercise1.java
@@ -0,0 +1,25 @@
+package recursion;
+
+/*Find the sum of digits of a positive number*/
+
+/*I am going to use recursion on this*/
+
+/*main class*/
+public class Exercise1 {
+
+    /*calculation method*/
+    static int operate(int n){
+        if (n == 0 || n < 0){
+            return 0;
+        }
+        return n % 10 + operate(n / 10);
+    }
+
+    /*main method*/
+    public static void main(String[] args) {
+
+        /*displaying the result*/
+        System.out.println(operate(111));
+    }
+
+}
