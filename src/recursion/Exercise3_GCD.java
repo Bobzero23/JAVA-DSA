05306d1d9ccb68abd3456c076b24399f62a3d22a
Bobzero <rajabu.mustafa.00@gmail.com>
2022-08-25 11:03:54 +0300

    GCD OF TWO NUMBERS

---
+++ src/recursion/Exercise3_GCD.java
@@ -0,0 +1,24 @@
+package recursion;
+
+/*Demonstration on how to find the GCD using recursion*/
+/* GCD -> Greatest Common Divisor*/
+
+/*main class*/
+public class Exercise3_GCD {
+
+    /*recursion method*/
+    static int gcd(int a, int b){
+        if (a < 0 || b < 0){
+            return -1;
+        }
+        if (b == 0){
+            return a;
+        }
+        return gcd(b, a % b);
+    }
+
+    /*main method*/
+    public static void main(String[] args) {
+        System.out.println("The GCD is: " + gcd(4,18));
+    }
+}
