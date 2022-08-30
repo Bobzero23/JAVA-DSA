bc917edb3d64af9ccc2a74e8cfa48def428383e5
Bobzero <rajabu.mustafa.00@gmail.com>
2022-08-30 10:06:01 +0300

    MORNING QUIZ COULDN'T DI IT

---
+++ src/Morning_quiz/PalindromicPartition_string.java
@@ -0,0 +1,26 @@
+package Morning_quiz;
+
+/*main class*/
+public class PalindromicPartition_string {
+
+    /*recursive method*/
+    public String partition(String str){
+        if (str.isEmpty()){
+            return "No String found";
+        }
+        if (str.length() == 1){
+            return str;
+        }
+
+        /*if the string length is greater than one leave space between each letter*/
+        /*first we have to find the palindromic partitions*/
+        /*then we separate the word from the other words*/
+        return  "";
+
+    }
+
+    /*main method*/
+    public static void main(String[] args) {
+
+    }
+}
