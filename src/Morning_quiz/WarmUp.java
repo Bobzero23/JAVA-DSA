f777841a66d2384a3333d48ab26a7b6fff1769c9
Bobzero <rajabu.mustafa.00@gmail.com>
2022-09-08 09:09:02 +0300

    WARMING UP

---
+++ src/Morning_quiz/WarmUp.java
@@ -0,0 +1,22 @@
+package Morning_quiz;
+
+/*main class*/
+public class WarmUp {
+
+    static int max1020(int a, int b) {
+        if (b > a) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        if (a >= 10 && a <= 20) return a;
+        if (b >= 10 && b <= 20) return b;
+        return 0;
+    }
+
+    /*main method*/
+    public static void main(String[] args) {
+
+        System.out.println(max1020(11, 20));
+    }
+}
