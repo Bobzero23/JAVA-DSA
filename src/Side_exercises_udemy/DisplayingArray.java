55f65c9ceb7e5fd8fb608667b3a3309eb9cf4171
Bobzero <rajabu.mustafa.00@gmail.com>
2022-09-09 10:15:57 +0300

    DISPLAYING AN ARRAY

---
+++ src/Side_exercises_udemy/DisplayingArray.java
@@ -0,0 +1,21 @@
+package Side_exercises_udemy;
+
+/*Here is a demonstration on how to display array without using for loop*/
+
+import java.util.Arrays;
+
+/*main class*/
+public class DisplayingArray {
+
+    /*main method*/
+    public static void main(String[] args) {
+
+        /*creating an array*/
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
+
+        /*displaying the array*/
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+}
