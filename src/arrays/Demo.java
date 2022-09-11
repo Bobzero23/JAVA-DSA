22916e21275df8617946d6ad45a00de461e734b0
Bobzero <rajabu.mustafa.00@gmail.com>
2022-09-11 15:12:47 +0300

    ARRAY - IMPLEMENTATION

---
+++ src/arrays/Demo.java
@@ -0,0 +1,29 @@
+package arrays;
+
+/*imports*/
+import java.util.Arrays;
+
+public class Demo {
+    public static void main(String[] args) {
+
+        int[] arr; // declaration
+
+        arr = new int[3]; // instantiation
+
+        /*initialization*/
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+
+        /*displaying the array*/
+        System.out.println(Arrays.toString(arr));
+
+        /*all together*/
+        String[] sArr = {"a", "b", "c"};
+
+        /*displaying the array*/
+        System.out.println(Arrays.toString(sArr));
+
+
+    }
+}
