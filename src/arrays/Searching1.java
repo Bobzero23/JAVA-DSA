8ee72875d9c085756aa0aa91bae681aef686b6a4
Bobzero <rajabu.mustafa.00@gmail.com>
2022-09-13 13:05:38 +0300

    ARRAY - IMPLEMENTATION - SEARCHING

---
+++ src/arrays/Searching1.java
@@ -0,0 +1,31 @@
+package arrays;
+
+/*main class*/
+public class Searching1 {
+
+    /*initializing an Array*/
+    int[] arr = {1, 2, 3, 4, 5, 6};
+
+    /*method to search index*/
+    public void search(int searched){
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] == searched){
+                System.out.println("The number you are searching is at index: " + i);
+                return;
+            }
+        }
+        System.out.println("We couldn't find the number: " + searched);
+    }
+
+    /*main method*/
+    public static void main(String[] args) {
+
+
+        /*creating object of the class*/
+        Searching1 object = new Searching1();
+
+        /*calling the method using the object*/
+        object.search(4);
+    }
+
+}
