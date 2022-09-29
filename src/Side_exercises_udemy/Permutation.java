c84b42dcb5798e360a5b3d5060e3911f60ca7432
Bobzero <rajabu.mustafa.00@gmail.com>
2022-09-29 10:00:25 +0300

    FINDING IF THE ARRAYS ARE PERMUTATION OF EACH OTHER OR NOT

--- src/Side_exercises_udemy/Permutation.java
+++ src/Side_exercises_udemy/Permutation.java
@@ -1,9 +1,8 @@
 package Side_exercises_udemy;
 
 /*Demonstration on how to find if the two arrays are permutation or not no matter if the values are in same index*/
-/*The logic I am going to use is that, I am going to loop twice and in the first loop I will take the firs element of
-* the first array and then checking if there is a same element in the second array and if there is not, it is
-* automatically not permutation..*/
+/*The logic I am going to use is that, as long as the length of these two arrays are same then if the sum and the
+* multiplication of the two arrays are same then these two arrays are permutation... but this only works for integers*/
 
 /*main class*/
 public class Permutation {
@@ -11,13 +10,30 @@ public class Permutation {
     /*method for finding permutation*/
     public boolean isPermutation(int[] arr1, int[] arr2){
 
-        for (int i = 0; i < arr1.length; i++){
-            for (int j = 0; j < arr2.length; j++){
-                if (arr1[i] == arr2[j]){
-                    return true;
-                }
-            }
+        /*checking if the lengths are same*/
+        if (arr1.length != arr2.length){
+            return false;
         }
+
+        /*creating variables*/
+        int sum1 = 0;
+        int sum2 = 0;
+        int mul1 = 1;
+        int mul2 = 1;
+
+        /*looping through the arrays*/
+        for (int i = 0; i < arr1.length; i++){
+            sum1 += arr1[i];
+            sum2 += arr2[i];
+            mul1 *= arr1[i];
+            mul2 *= arr2[i];
+        }
+
+        /*checking the results conditions*/
+        if ( sum1 == sum2 && mul1 == mul2){
+            return true;
+        }
+
         return false;
     }
 
@@ -29,7 +45,7 @@ public class Permutation {
 
         /*instantiating the arrays*/
         int[] arr1 = {1, 2, 3, 4, 5};
-        int[] arr2 = {1, 3, 4, 2, 6};
+        int[] arr2 = {1, 3, 4, 2, 5};
 
         /*instantiating the result variable*/
         boolean result = object.isPermutation(arr1, arr2);
