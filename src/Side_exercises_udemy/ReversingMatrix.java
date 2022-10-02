9f4ac55fa065f9f8a287700308f8a516b3c9483f
Bobzero <rajabu.mustafa.00@gmail.com>
2022-10-02 21:35:13 +0300

    REVIEWING STRINGS

---
+++ src/Side_exercises_udemy/ReversingMatrix.java
@@ -0,0 +1,18 @@
+package Side_exercises_udemy;
+
+/*Demonstration on how to reverse a matrix*/
+/*First thing first, you need to check if the matrix is nxn or the row is not 0*/
+
+/*main class*/
+public class ReversingMatrix {
+
+    /*method for reversing a matrix*/
+
+
+
+    /*main method*/
+    public static void main(String[] args) {
+
+        /**/
+    }
+}
--- src/Test/Attempt.java
+++ src/Test/Attempt.java
@@ -1,20 +1,44 @@
 package Test;
 
-/*what the hell is this code doing mehn*/
-
-import java.util.Locale;
-
 /*main class*/
 public class Attempt {
 
+    /*first method*/
+    public String method1(String str1){
+        String back = str1.substring(str1.length() - 4, str1.length());
+        String front = str1.substring(1, str1.length() - 4);
+        return "back is: " + back + "\nfront is: " + front;
+    }
+
+    /*second method*/
+    public String method2 (String str2){
+        return str2.substring(1, str2.length()-1);
+    }
 
     /*main method*/
     public static void main(String[] args) {
 
+        /*creating the object of the class*/
+        Attempt object = new Attempt();
+
+        /*instantiating the string*/
         String str = "Bobzero";
-        String back = str.substring(str.length() - 3, str.length());
-        System.out.println(back);
-        String front = str.substring(0, str.length() - 3);
-        System.out.println(front);
+
+        /*calling method1 in a variable*/
+        String result1 = object.method1(str);
+
+        /*displaying the result*/
+        System.out.println(result1);
+
+        /*Instantiating the string*/
+        String str2 = "Bobzero";
+
+        /*calling the method2 in a variable*/
+        String result2 = object.method2(str2);
+
+        /*displaying the result*/
+        System.out.println(result2);
+
+
     }
 }
