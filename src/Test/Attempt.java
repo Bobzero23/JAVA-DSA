e6ecfcf0a6e65f4c4750fb5031427ac66f5be0b6
Bobzero <rajabu.mustafa.00@gmail.com>
2022-10-12 10:40:56 +0300

    A LITTLE WARM UP

--- src/Test/Attempt.java
+++ src/Test/Attempt.java
@@ -4,15 +4,21 @@ package Test;
 public class Attempt {
 
     /*first method*/
-    public String method1(String str1){
-        String back = str1.substring(str1.length() - 4, str1.length());
-        String front = str1.substring(1, str1.length() - 4);
-        return "back is: " + back + "\nfront is: " + front;
+    public void method1(String str1){
+        System.out.println(str1.indexOf("x"));
     }
 
-    /*second method*/
-    public String method2 (String str2){
-        return str2.substring(1, str2.length()-1);
+    boolean doubleX(String str) {
+        int i = str.indexOf("x");
+        if (i == -1) return false; // no "x" at all
+
+        // Is char at i+1 also an "x"?
+        if (i+1 >= str.length()) return false; // check i+1 in bounds?
+        return str.substring(i+1, i+2).equals("x");
+
+        // Another approach -- .startsWith() simplifies the logic
+        // String x = str.substring(i);
+        // return x.startsWith("xx");
     }
 
     /*main method*/
@@ -22,23 +28,13 @@ public class Attempt {
         Attempt object = new Attempt();
 
         /*instantiating the string*/
-        String str = "Bobzero";
+        String str = "taxing";
 
-        /*calling method1 in a variable*/
-        String result1 = object.method1(str);
+        /*calling method 1*/
+        object.method1(str);
 
-        /*displaying the result*/
-        System.out.println(result1);
-
-        /*Instantiating the string*/
-        String str2 = "Bobzero";
-
-        /*calling the method2 in a variable*/
-        String result2 = object.method2(str2);
-
-        /*displaying the result*/
-        System.out.println(result2);
-
+        /*calling method 2*/
+        object.doubleX(str);
 
     }
 }
