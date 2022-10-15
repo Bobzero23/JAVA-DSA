019109643d10a076b1e5c53746399e94e2c865a4
Bobzero <rajabu.mustafa.00@gmail.com>
2022-10-15 09:09:14 +0300

    MORNING WARMUP - DONE

--- src/Test/Attempt.java
+++ src/Test/Attempt.java
@@ -1,18 +1,38 @@
 package Test;
 
-/*Demonstration on how to solve the Given a string, return the count of the number of times that
-a substring length 2 appears in the string and also as the last 2 chars of the string,
-so "hixxxhi" yields 1 (we won't count the end substring). */
+/*Demonstration on how to solve the Given array, return true if sequence of 1,2,3 appears somewhere in the array.
 
 /*main class*/
 public class Attempt {
 
+    /*method to solve the question*/
+    public boolean solver(int[] arr){
+        if (arr.length < 3) return false;
+        for (int i = 0; i < arr.length - 2; i++){
+            if (arr[i] == 1){
+                if (arr[i + 1] == 2){
+                    if (arr[i + 2] == 3){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
 
     /*main method*/
     public static void main(String[] args) {
-
-        /*creating the object of the class*/
+        /*creating object of the class*/
         Attempt object = new Attempt();
+
+        /*creating the array*/
+        int[] arr = {1, 1, 2, 4, 1};
+
+        /*assigning the method to a variable*/
+        boolean result = object.solver(arr);
+
+        /*displaying the result*/
+        System.out.println(result);
 
     }
 }
