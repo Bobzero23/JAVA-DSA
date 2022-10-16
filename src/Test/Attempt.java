a60cd146658793e28360c6d6ff0c01e91bf4c353
Bobzero <rajabu.mustafa.00@gmail.com>
2022-10-16 08:52:45 +0300

    MORNING WARMUP

--- src/Test/Attempt.java
+++ src/Test/Attempt.java
@@ -1,40 +1,46 @@
 package Test;
 
-/*Demonstration on how to solve the Given String, Count the number of "xx" in the given string.
-We'll say that overlapping is allowed, so "xxx" contains 2 "xx" */
+/*Given an array of ints, return true if it contains a 2, 7, 1 pattern: a value,followed by the value plus 5,
+followed by the value minus 1. Additionally, the 271 counts even if the "1" differs by 2 or less from the correct value.*/
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /*main class*/
 public class Attempt {
 
     /*method to solve the question*/
-    public int solver(String str){
-        /*initializing variables*/
-        int count = 0;
-
-        /*checking the String length*/
-        if (str.length() < 2) return 0;
-
-        /*looping through the characters of String*/
-        for (int i = 0; i < str.length() - 1; i++){
-            if (str.charAt(i) == 'x' && str.charAt(i + 1) == 'x'){
-                count = count + 1;
+        public int[] twoSum(int[] nums, int target) {
+            int n=nums.length;
+            Map<Integer,Integer> map=new HashMap<>();
+            int[] result=new int[2];
+            for(int i=0;i<n;i++){
+                if(map.containsKey(target-nums[i])){
+                    result[1]=i;
+                    result[0]=map.get(target-nums[i]);
+                    return result;
+                }
+                map.put(nums[i],i);
             }
+            return result;
         }
-        return count;
-    }
+
 
     /*main method*/
     public static void main(String[] args) {
         /*creating object of the class*/
         Attempt object = new Attempt();
 
-        /*initializing the string*/
-        String str = "Hexxo thxxe";
+        /*creating an array*/
+        int[] nums = {3, 3};
 
-        /*assigning the method to a variable*/
-        int result = object.solver(str);
+        /*assigning the method to an array*/
+        int[] result = object.twoSum(nums, 6);
 
         /*displaying the result*/
-        System.out.println(result);
+        System.out.println(Arrays.toString(result));
+
+
     }
 }
