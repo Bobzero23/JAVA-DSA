6d3529ae8e2046531359c193ca4c398f942c225c
Bobzero <rajabu.mustafa.00@gmail.com>
2022-11-20 10:24:55 +0300

    STRING SENTENCE TO ARRAY

--- src/Test/Attempt1.java
+++ src/Test/Attempt1.java
@@ -1,53 +1,24 @@
 package Test;
 
-/**Demonstration on how to check if the strings are isomorphic*/
+import java.util.Arrays;
+
+/**Converting a sentence to array*/
 
 
 /*imports*/
-import java.util.HashMap;
-import java.util.Map;
+
 
 /*main class*/
 public class Attempt1 {
 
-    /*solver method*/
-    private String transformString(String s) {
-        Map<Character, Integer> indexMapping = new HashMap<>();
-        StringBuilder builder = new StringBuilder();
-
-        for (int i = 0; i < s.length(); ++i) {
-            char c1 = s.charAt(i);
-
-            if (!indexMapping.containsKey(c1)) {
-                indexMapping.put(c1, i);
-            }
-
-            builder.append(Integer.toString(indexMapping.get(c1)));
-            builder.append(" ");
-        }
-        return builder.toString();
-    }
-
-    /*control method*/
-    public boolean isIsomorphic(String s, String t) {
-        return transformString(s).equals(transformString(t));
-    }
-
     /*main method*/
     public static void main(String[] args) {
 
+        String sentence = "This is a simple sentence";
+        String[] words = sentence.split(" ");
+        System.out.println(Arrays.toString(words));
+
         //creating object of the class
         Attempt1 object = new Attempt1();
-
-        //creating strings
-        String s = "egg";
-        String t = "add";
-
-        //calling a method in a variable
-        boolean result = object.isIsomorphic(t, s);
-
-        //displaying the result
-        System.out.println(result);
-
     }
 }
