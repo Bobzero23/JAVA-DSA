71bce9195dcfd8460dab49e13f5f3c43bfcdfdf2
Bobzero <rajabu.mustafa.00@gmail.com>
2022-12-02 09:21:31 +0300

    3 SUM WITH HASHSET DONE

--- src/Test/Attempt1.java
+++ src/Test/Attempt1.java
@@ -15,33 +15,6 @@ public class Attempt1 {
 
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> output = new LinkedList<>();
-        Arrays.sort(nums);
-
-        for(int i = 0; i < nums.length - 2; i++) {
-            int start = i  + 1; int end = nums.length - 1;
-
-            if(i == 0 || nums[i] != nums[i - 1]) {
-                while(start < end) {
-                    int current = nums[i] + nums[start] + nums[end];
-
-                    if(current== 0) {
-                        output.add(Arrays.asList(nums[i], nums[start], nums[end]));
-
-                        while(start < end && nums[start] == nums[start + 1]) start++;
-                        while(start < end && nums[end] == nums[end - 1]) end--;
-
-                        start++;
-                        end--;
-
-                    }else if( current > 0) {
-                        end--;
-                    }else {
-                        start++;
-                    }
-                }
-            }
-
-        }
 
         return output;
     }
