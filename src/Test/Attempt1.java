8b2408ad4b91e17114bb5b40b48c268465d046c6
Bobzero <rajabu.mustafa.00@gmail.com>
2022-12-05 09:20:56 +0300

    STRING PRACTISES

--- src/Test/Attempt1.java
+++ src/Test/Attempt1.java
@@ -13,55 +13,16 @@ public class Attempt1 {
 
     /*main method*/
 
-    public String longestPalindrome(String str) {
 
-        if (str.length() <= 1)
-            return str;
-
-        String LPS = "";
-
-        for (int i = 1; i < str.length(); i++) {
-
-            // Consider odd length
-            int low = i;
-            int high = i;
-            while(str.charAt(low) == str.charAt(high)) {
-                low--;
-                high++;
-
-                if (low == -1 || high == str.length())
-                    break;
-            }
-
-            String palindrome = str.substring(low+1, high);
-            if (palindrome.length() > LPS.length()) {
-                LPS = palindrome;
-            }
-
-            // Consider even length
-            low = i-1;
-            high = i;
-            while(str.charAt(low) == str.charAt(high)) {
-                low--;
-                high++;
-
-                if (low == -1 || high == str.length())
-                    break;
-            }
-
-            palindrome = str.substring(low+1, high);
-            if (palindrome.length() > LPS.length()) {
-                LPS = palindrome;
-            }
-        }
-
-        return LPS;
-    }
 
 
     public static void main(String[] args) {
         Attempt1 object = new Attempt1();
 
+        String str = "Bobzero";
+
+        String last = str.substring(str.length() - 1, str.length());
+        System.out.println(last);
 
     }
 }
