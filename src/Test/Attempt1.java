07b35d7924c0a3284cbbcff1388c81df47896757
Bobzero <rajabu.mustafa.00@gmail.com>
2022-12-11 10:28:20 +0300

    4 SUM FAILURE

--- src/Test/Attempt1.java
+++ src/Test/Attempt1.java
@@ -2,27 +2,44 @@ package Test;
 
 import java.util.*;
 
-/** sorting a sentence */
-
-
-/*imports*/
+/** 4 sum */
 
 
 /*main class*/
 public class Attempt1 {
 
+    /*method*/
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Set<List<Integer>> set = new HashSet<>();
+
+        Arrays.sort(nums);
+
+        for(int i = 0; i < nums.length - 3; i++) {
+            int leftPointer = i + 1;
+            int rightPointer = nums.length - 1;
+
+            while(leftPointer < rightPointer) {
+                if(nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] == target) {
+                    set.add(Arrays.asList(nums[i], nums[i + 1], nums[i + 2], nums[i + 3]));
+                    rightPointer--;
+                    leftPointer++;
+                }
+            }
+        }
+
+        return new ArrayList(set);
+    }
+
     /*main method*/
-
-
-
-
     public static void main(String[] args) {
         Attempt1 object = new Attempt1();
 
-        String str = "Bobzero";
+        int target = 0;
+        int[] nums = {1,0,-1,0,-2,2};
 
-        String last = str.substring(str.length() - 1, str.length());
-        System.out.println(last);
+        List<List<Integer>> result =  object.fourSum(nums, target);
+
+        System.out.println(result);
 
     }
 }
