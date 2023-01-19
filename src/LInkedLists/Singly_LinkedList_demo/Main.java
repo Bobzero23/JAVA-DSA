f2efe184bf9400b12ba58cbbebb19eba8ffa73f0
Bobzero <rajabu.mustafa.00@gmail.com>
2023-01-19 21:41:12 +0300

    LINKED LIST INSERTION

--- src/LInkedLists/Singly_LinkedList_demo/Main.java
+++ src/LInkedLists/Singly_LinkedList_demo/Main.java
@@ -3,8 +3,11 @@ package LInkedLists.Singly_LinkedList_demo;
 public class Main {
     public static void main(String[] args) {
         Singly_LinkedList sLL = new Singly_LinkedList();
-        sLL.create_singly_linkedlist(5);
-
+        sLL.create_singly_linkedList(5);
         System.out.println(sLL.head.value);
+        sLL.insert_into_linked_list(10, 1);
+        System.out.println(sLL.head.next.value);
+        sLL.insert_into_linked_list(20, 3);
+        System.out.println(sLL.head.next.next.value);
     }
 }
--- src/LInkedLists/Singly_LinkedList_demo/Singly_LinkedList.java
+++ src/LInkedLists/Singly_LinkedList_demo/Singly_LinkedList.java
@@ -8,7 +8,8 @@ public class Singly_LinkedList {
     public int size;
 
 
-    public Node create_singly_linkedlist(int node_value) {
+    /*method to create a singly linked list*/
+    public Node create_singly_linkedList(int node_value) {
         head = new Node();
         Node node = new Node();
         node.next = null;
@@ -17,5 +18,34 @@ public class Singly_LinkedList {
         tail = node;
         size = 1;
         return  head;
+    }
+
+    /*method to insert a value to a singly linked list*/
+    public void insert_into_linked_list(int node_value, int location) {
+        Node node = new Node();
+        node.value = node_value;
+
+        /*if there is no linked list yet, then we create a linked list*/
+        if(head == null) {
+            create_singly_linkedList(node_value);
+            return;
+        }else if (location == 0) { //inset at the beginning
+            node.next = head;
+            head = node;
+        }else if (location >= size) { //insert at the end
+            node.next = null;
+            tail.next = node;
+            tail = node;
+        }else{ //insert anywhere
+            Node temp_node = head;
+            int index = 0;
+            while(index < location - 1) {
+                temp_node = temp_node.next;
+            }
+            Node next_node = temp_node.next;
+            temp_node.next = node;
+            node.next = next_node;
+        }
+        size++;
     }
 }
