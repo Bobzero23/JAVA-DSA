a189ef920b2903d25b5966f2c02a4145cdf6db83
Bobzero <rajabu.mustafa.00@gmail.com>
2023-02-13 12:46:55 +0300

    CIRCULAR LINKED LIST INSERTION

--- src/LInkedLists/circularSinglyLinkedList/CircularSinglyLinkedList.java
+++ src/LInkedLists/circularSinglyLinkedList/CircularSinglyLinkedList.java
@@ -5,6 +5,7 @@ public class CircularSinglyLinkedList {
     public Node tail;
     public int size;
 
+    /**method to create circular singly linked list (csll)*/
     public Node createCSLL (int nodeValue) {
         head = new Node();
         Node node = new Node();
@@ -14,5 +15,34 @@ public class CircularSinglyLinkedList {
         tail = node;
         size = 1;
         return head;
+    }
+
+    /**method to insert a node into a circular singly linked list*/
+    public void insertCSLL(int nodeValue, int location) {
+        Node node = new Node(); //creating the new node
+        node.value = nodeValue; //initializing the value
+
+        if (head == null) { //if there is no linked list yet,then we create it
+            createCSLL(nodeValue);
+            return;
+        }else if (location == 0) { //inserting to the beginning
+            node.next = head;
+            head = node;
+            tail.next = node;
+        }else if (location >= size) { //inserting to the end
+            tail.next = node;
+            tail = node;
+            tail.next = head;
+        } else { //inserting anywhere else
+            Node tempNode = new Node();
+            int index = 0;
+            while (index < location - 1) {
+                tempNode = tempNode.next;
+                index++;
+            }
+            node.next = tempNode.next;
+            tempNode.next = node;
+            size++;
+        }
     }
 }
--- src/Test/test3/Main.java
+++ src/Test/test3/Main.java
@@ -1,145 +1,29 @@
 package Test.test3;
 
 public class Main {
-    public static void main(String[] args) {
-        Singly_Linked_List sll_object = new Singly_Linked_List();
-        sll_object.insert_node(9, 0);
-        sll_object.insert_node(10, 1);
-        sll_object.insert_node(50, 2);
-        sll_object.insert_node(68, 3);
-        sll_object.insert_node(59, 4);
-        sll_object.insert_node(26, 5);
-        sll_object.insert_node(95, 6);
-        sll_object.traversal();
-        sll_object.search(68);
-        sll_object.delete(3);
-        sll_object.traversal();
-        sll_object.search(68);
-    }
 
-    public static class Singly_Linked_List{
+    public static class CSLL {
         public Node head;
         public Node tail;
         public int size;
 
-        /**METHOD TO CREATE A NODE IN SINGLY LINKED LIST*/
-        public Node create_node(int node_value) {
+        public Node createCSLL(int nodeValue) {
             Node node = new Node();
-            node.next = null;
-            node.value = node_value;
+            node.value = nodeValue;
+            node.next = node;
             head = node;
             tail = node;
-            size = 1;
             return head;
-        }
-
-        /**METHOD TO INSERT A NODE IN A SINGLY LINKED LIST*/
-        public void insert_node(int node_value, int node_location) {
-            Node node = new Node();
-            node.value = node_value;
-
-            if (head == null) {
-                create_node(node_value);
-                return;
-            }
-            if(node_location == 0) {
-                node.next = head;
-                tail = node;
-
-            }else if (node_location >= size){
-                node.next = null;
-                tail.next = node;
-                tail = node;
-            }else {
-                Node temp_node = new Node();
-                int index = 0;
-
-                while (index < node_location - 1) {
-                    temp_node = temp_node.next;
-                }
-
-                Node next_node = new Node();
-                temp_node.next = node;
-                node.next = next_node;
-            }
-            size++;
-        }
-
-        /**METHOD TO TRAVERSAL THE SINGLY LINKED LIST*/
-        public void traversal() {
-            if (head == null) {
-                System.out.println("The linked list is empty..");
-            }else {
-                Node temp_node = head;
-                for (int i = 0; i < size; i++) {
-                    System.out.print(temp_node.value);
-                    if (i != size - 1) {
-                        System.out.print(" -> ");
-                    }
-                    temp_node = temp_node.next;
-                }
-                System.out.println();
-            }
-        }
-
-        /**METHOD TO SEARCH FOR A NODE IN SINGLY LINKED LIST*/
-        boolean search(int nodeToSearch) {
-            if (head == null) {
-                System.out.println("The linked list is empty");
-            }else {
-                Node temp_node = head;
-                for (int i = 0; i < size; i++) {
-                    if (temp_node.value == nodeToSearch) {
-                        System.out.println("Node found at location " + i + "\n");
-                        return true;
-                    }
-                    temp_node = temp_node.next;
-                }
-            }
-            System.out.println("The node you are searching is not Available");
-            return false;
-        }
-
-        /**METHOD TO DELETE A NODE IN SINGLY LINKED LIST*/
-        public void delete(int location) {
-            if (head == null) {
-                System.out.println("Linked list is empty");
-                return;
-            }else if (location == 0) { // deleting the first element
-                    head = head.next;
-                    size--;
-                    if (size == 0) { // deleted element was the only element we had
-                        tail = null;
-                    }
-            }else if (location >= size - 1) { // deleting the last element
-                Node temp_node = head;
-                for (int i = 0; i < size; i++) {
-                    temp_node = temp_node.next;
-                }
-                if (temp_node == head) { // the last element deleted was the only element we had
-                    head = tail = null;
-                    size--;
-                    return;
-                }
-            }else{
-                Node temp_node = head;
-                for (int i = 0; i < location - 1; i++) {
-                    temp_node = temp_node.next;
-                }
-                temp_node.next = temp_node.next.next;
-                size--;
-            }
-        }
-
-        public void deleteEntireLinkedList() {
-            head = null;
-            tail = null;
-            System.out.println("The entire linked list is deleted successfully..");
         }
     }
 
-    public static class Node{
+    public static class Node {
         public int value;
         public Node next;
+    }
+    public static void main(String[] args) {
+        CSLL object = new CSLL();
+        object.createCSLL(10);
+        System.out.println(object.head.next.value);
     }
 }
