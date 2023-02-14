40ada9ac156aab1b7b3f2cad5f3537680eb32f76
Bobzero <rajabu.mustafa.00@gmail.com>
2023-02-14 12:27:05 +0300

    LEETCODE NOT DONE

---
+++ src/LeetCode/AddBinary_67.java
@@ -0,0 +1,12 @@
+package LeetCode;
+
+public class AddBinary_67 {
+    public String addBinary(String a, String b) {
+        StringBuilder result = new StringBuilder();
+        int aLength = a.length();
+        int bLength = b.length();
+        int carry = 0; //in case there is carrying while adding the binary
+
+
+    }
+}
