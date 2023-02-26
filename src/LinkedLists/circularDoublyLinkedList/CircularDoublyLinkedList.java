1928c63b0979f2463bf32854381cbd27c7318c4a
Bobzero <rajabu.mustafa.00@gmail.com>
2023-02-26 11:09:11 +0300

    CIRCULAR DOUBLY LINKED LIST INSERTION DONE

--- src/LinkedLists/circularDoublyLinkedList/CircularDoublyLinkedList.java
+++ src/LinkedLists/circularDoublyLinkedList/CircularDoublyLinkedList.java
@@ -6,7 +6,7 @@ public class CircularDoublyLinkedList {
     public int size;
 
     /**method to create a circular doubly linked list*/
-    public Node creatCDLL(int nodeValue) {
+    public Node createCDLL(int nodeValue) {
         Node newNode = new Node();
         newNode.value = nodeValue;
         newNode.next = newNode;
@@ -15,5 +15,38 @@ public class CircularDoublyLinkedList {
         tail = newNode;
         size = 1;
         return head;
+    }
+
+    /**insertion in circular doubly linked list*/
+    public void insertToCDLL(int location, int nodeValue) {
+        Node newNode = new Node();
+        newNode.value = nodeValue;
+
+        if(head == null) {
+            createCDLL(nodeValue);
+            return;
+        }else if(location == 0) {
+            newNode.next = head;
+            newNode.prev = tail;
+            head.prev = newNode;
+            tail.next = newNode;
+            head = newNode;
+        }else if(location >= size) {
+            newNode.next = head;
+            newNode.prev = tail;
+            head.prev = newNode;
+            tail.next = newNode;
+            tail = newNode;
+        }else {
+            Node tempNode = head;
+            for (int i = 0; i < location - 1; i++) {
+                tempNode = tempNode.next;
+            }
+            newNode.prev = tempNode;
+            newNode.next = tempNode.next;
+            tempNode.next = newNode;
+            newNode.next.prev = newNode;
+        }
+        size++;
     }
 }
--- src/LinkedLists/circularDoublyLinkedList/Main.java
+++ src/LinkedLists/circularDoublyLinkedList/Main.java
@@ -3,7 +3,18 @@ package LinkedLists.circularDoublyLinkedList;
 public class Main {
     public static void main(String[] args) {
         CircularDoublyLinkedList cDLL_Object = new CircularDoublyLinkedList();
-        cDLL_Object.creatCDLL(9);
+        cDLL_Object.createCDLL(9);
         System.out.println(cDLL_Object.head.value);
+
+        cDLL_Object.insertToCDLL(0, 0);
+        cDLL_Object.insertToCDLL(1, 1);
+        cDLL_Object.insertToCDLL(2, 2);
+        cDLL_Object.insertToCDLL(3, 3);
+        cDLL_Object.insertToCDLL(4, 4);
+        cDLL_Object.insertToCDLL(5, 5);
+        cDLL_Object.insertToCDLL(6, 6);
+        cDLL_Object.insertToCDLL(7, 7);
+        cDLL_Object.insertToCDLL(8, 8);
+
     }
 }
