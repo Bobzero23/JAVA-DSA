112ee822f0dc1e1e0b104f75563421820d937e68
Bobzero <rajabu.mustafa.00@gmail.com>
2023-03-24 11:02:40 +0300

    debugging leetcode question

--- src/Test/test3/Main.java
+++ src/Test/test3/Main.java
@@ -1,44 +1,7 @@
 package Test.test3;
 
-public class Main {
-
-    public static class CSLL {
-        public Node head;
-        public Node tail;
-        public int size;
-
-        /**method to create a circular singly linked list*/
-        public Node createCSLL(int nodeValue) {
-            Node node = new Node();
-            node.value = nodeValue;
-            node.next = node;
-            head = node;
-            tail = node;
-            return head;
-        }
-
-        /**method to insert a node into circular singly linked list*/
-        public void insertCSLL(int nodeValue, int location) {
-            Node node = new Node();
-            node.value = nodeValue;
-
-            if (head == null) {
-                createCSLL(nodeValue);
-                return;
-            }else  if (location == 0) {
-                head.next = node;
-                head = node;
-            }
-        }
-    }
-
-    public static class Node {
-        public int value;
-        public Node next;
-    }
+class Main{
     public static void main(String[] args) {
-        CSLL object = new CSLL();
-        object.createCSLL(10);
-        System.out.println(object.head.next.value);
+        System.out.println(9 % 10);
     }
 }
