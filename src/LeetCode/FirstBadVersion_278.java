92fb29dd294194ecc4e2a3fde8ac7917ba439c72
Bobzero <rajabu.mustafa.00@gmail.com>
2023-04-01 07:18:17 +0300

    leetcoding

---
+++ src/LeetCode/FirstBadVersion_278.java
@@ -0,0 +1,23 @@
+package LeetCode;
+
+public class FirstBadVersion_278 {
+    boolean isBadVersion(int version) {
+        return version >= 10;
+    }
+}
+
+/* this code is not completed so be aware*/
+
+class solution extends FirstBadVersion_278 {
+    public int firstBadVersion(int n) {
+        boolean isFound = false;
+
+        while((n >= 1) && !isFound) {
+            if(isBadVersion(n)) {
+                n--;
+            }else if (!super.isBadVersion(n)){
+                n++;
+            }else {
+        }
+    }
+}
