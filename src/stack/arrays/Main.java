8feb62e705eb14262bae685806e8554ab6038a6e
Bobzero <rajabu.mustafa.00@gmail.com>
2023-04-24 11:58:59 +0300

    stack with array completed

--- src/stack/arrays/Main.java
+++ src/stack/arrays/Main.java
@@ -3,9 +3,20 @@ package stack.arrays;
 public class Main {
     public static void main(String[] args) {
         Stack stack = new Stack(5);
+
         boolean isStackEmpty = stack.isEmpty();
         System.out.println(isStackEmpty);
+
         boolean isStackFull = stack.isFull();
         System.out.println(isStackFull);
+
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+        stack.push(6);
+
+        stack.pop();
     }
 }
--- src/stack/arrays/Stack.java
+++ src/stack/arrays/Stack.java
@@ -17,4 +17,33 @@ public class Stack {
     public boolean isFull(){
         return topOfStack == array.length - 1;
     }
+
+    public void push(int value) {
+        if (isFull()) {
+            System.out.println("The stack is full!");
+        }else {
+            topOfStack++;
+            array[topOfStack] = value;
+            System.out.println("The value " +value+ " was successfully added to the stack.");
+        }
+    }
+
+    public  void pop() {
+        if (isEmpty()) {
+            System.out.println("The stack is empty!");
+            return;
+        }else {
+            topOfStack--;
+            System.out.println("The element " + array[topOfStack] + " was deleted successfully.");
+        }
+    }
+
+    public int peek() {
+        if (isEmpty()) {
+            System.out.println("The stack is empty!");
+            return -1;
+        }else {
+            return array[topOfStack];
+        }
+    }
 }
