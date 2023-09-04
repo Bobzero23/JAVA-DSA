993de11801cc7ef067a990647f210cbc82c0f313
Bobzero23 <rajabu.mustafa.00@gmail.com>
2023-09-04 13:40:31 +0300

    leetcoding trapping rain water

--- src/Test/test1/Attempt2.java
+++ src/Test/test1/Attempt2.java
@@ -11,14 +11,22 @@ public class Attempt2 {
     public static void main(String[] args) {
         int max = 0;
 
-        int[] height = {0,1,0,2,1,0,1,3,2,1,2,1};
-        int[] left = new int[height.length];
+        int[] heights = {0,1,0,2,1,0,1,3,2,1,2,1};
+        int[] left = new int[heights.length];
+        int[] right = new int[heights.length];
 
-        for(int i = 0; i < height.length; i++) {
-            max = Math.max(max, height[i]);
+        for(int i = 0; i < heights.length; i++) {
+            max = Math.max(max, heights[i]);
             left[i] = max;
         }
 
+        max = heights[heights.length - 1];
+        for (int i = heights.length - 1; i >= 0; i--) {
+            right[i] = Math.max(heights[i], max);
+            max = right[i];
+        }
+
         System.out.println(Arrays.toString(left));
+        System.out.println(Arrays.toString(right));
     }
 }
