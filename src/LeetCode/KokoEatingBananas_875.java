5bab5cc0f9aaa763997587448eedb79900501f72
Bobzero <rajabu.mustafa.00@gmail.com>
2023-09-19 15:20:38 +0300

    leetcoding koko eating bananas

---
+++ src/LeetCode/KokoEatingBananas_875.java
@@ -0,0 +1,41 @@
+package LeetCode;
+
+public class KokoEatingBananas_875 {
+
+    public static  int minEatingSpeed(int[] piles, int h) {
+        // Initialize the left and right boundaries
+        int left = 1, right = 1;
+        for (int pile : piles) {
+            right = Math.max(right, pile);
+        }
+
+        while (left < right) {
+            // Get the middle index between left and right boundary indexes.
+            // hourSpent stands for the total hour Koko spends.
+            int middle = (left + right) / 2;
+            int hourSpent = 0;
+
+            // Iterate over the piles and calculate hourSpent.
+            // We increase the hourSpent by ceil(pile / middle)
+            for (int pile : piles) {
+                hourSpent += Math.ceil((double) pile / middle);
+            }
+
+            // Check if middle is a workable speed, and cut the search space by half.
+            if (hourSpent <= h) {
+                right = middle;
+            } else {
+                left = middle + 1;
+            }
+        }
+
+        // Once the left and right boundaries coincide, we find the target value,
+        // that is, the minimum workable eating speed.
+        return right;
+    }
+    public static void main(String[] args) {
+        int[] piles = {3,6,7,11};
+        int result = minEatingSpeed(piles, 8);
+        System.out.println(result);
+    }
+}
--- src/LeetCode/SearchInA2DMatrix_74.java
+++ src/LeetCode/SearchInA2DMatrix_74.java
@@ -1,24 +1,49 @@
 package LeetCode;
 
 public class SearchInA2DMatrix_74 {
-    public static boolean searchMatrix(int[][] matrix, int target) {
-        int i = 0, j = matrix[0].length - 1;
 
-        while (i < matrix.length && j >= 0) {
-            if (matrix[i][j] == target) {
+    //    Time: O(log(mn)) | Space: O(1)
+    public static boolean searchMatrix2(int[][] matrix, int target) {
+        if(matrix.length == 0) return false;
+
+        int rows = matrix.length;
+        int columns = matrix[0].length;
+
+        int low = 0;
+        int high = rows * columns;
+
+        while(low < high) {
+            int mid = (low+high)/2;
+
+            if(matrix[mid/columns][mid%columns] == target) {
                 return true;
-            } else if (matrix[i][j] > target) {
-                j--;
+            } else if (matrix[mid/columns][mid%columns] < target) {
+                low = mid+1;
             } else {
-                i++;
+                high = mid;
             }
         }
         return false;
     }
 
+//    public static boolean searchMatrix(int[][] matrix, int target) {
+//        int i = 0, j = matrix[0].length - 1;
+//
+//        while (i < matrix.length && j >= 0) {
+//            if (matrix[i][j] == target) {
+//                return true;
+//            } else if (matrix[i][j] > target) {
+//                j--;
+//            } else {
+//                i++;
+//            }
+//        }
+//        return false;
+//    }
+
     public static void main(String[] args) {
         int[][] matrix = {{1,3,5,7}, {10,11,16,20}, {23,30,34,60} };
-        boolean result = searchMatrix(matrix, 3);
+        boolean result = searchMatrix2(matrix, 3);
         System.out.println(result);
     }
 }
