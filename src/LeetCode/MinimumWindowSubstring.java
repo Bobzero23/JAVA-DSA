5334904b2bdb29efec4408b93b8fcc130d73cf6c
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-02-14 20:55:12 +0300

    debuggin minimun window substring

---
+++ src/LeetCode/MinimumWindowSubstring.java
@@ -0,0 +1,47 @@
+package LeetCode;
+
+import java.util.HashMap;
+
+public class MinimumWindowSubstring {
+    public static String minWindow(String s, String t) {
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (char x : t.toCharArray()) {
+            map.put(x, map.getOrDefault(x, 0) + 1);
+        }
+
+        int matched = 0;
+        int start = 0;
+        int minLen = s.length() + 1;
+        int subStr = 0;
+
+        for (int endWindow = 0; endWindow < s.length(); endWindow++) {
+            char right = s.charAt(endWindow);
+            if (map.containsKey(right)) {
+                map.put(right, map.get(right) - 1);
+                if (map.get(right) == 0) matched++;
+            }
+
+            while (matched == map.size()) {
+                if (minLen > endWindow - start + 1) {
+                    minLen = endWindow - start + 1;
+                    subStr = start;
+                }
+                char deleted = s.charAt(start++);
+                if (map.containsKey(deleted)) {
+                    if (map.get(deleted) == 0) matched--;
+                    map.put(deleted, map.get(deleted) + 1);
+                }
+            }
+        }
+        return minLen > s.length() ? "" : s.substring(subStr, subStr + minLen);
+    }
+
+    public static void main(String[] args) {
+        String t = "ABC";
+        String s = "ADOBECODEBANC";
+
+        String result = minWindow(s, t);
+        System.out.println(result);
+    }
+}
