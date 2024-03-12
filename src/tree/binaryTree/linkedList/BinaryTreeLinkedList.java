5a2f76dbb62e5e57dd5b137cdae184d60aebd67c
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-03-12 17:55:48 +0300

    binary tree pre-order traversal

--- src/tree/binaryTree/linkedList/BinaryTreeLinkedList.java
+++ src/tree/binaryTree/linkedList/BinaryTreeLinkedList.java
@@ -6,4 +6,12 @@ public class BinaryTreeLinkedList {
     public BinaryTreeLinkedList() {
         this.root = null;
     }
+
+    /*pre-order traversal*/
+    void preOrder(Node node) {
+        if (node == null) return;
+        System.out.print(node.value + " -> ");
+        preOrder(node.left);
+        preOrder(node.right);
+    }
 }
--- src/tree/binaryTree/linkedList/Main.java
+++ src/tree/binaryTree/linkedList/Main.java
@@ -3,5 +3,35 @@ package tree.binaryTree.linkedList;
 public class Main {
     public static void main(String[] args) {
         BinaryTreeLinkedList binaryTree = new BinaryTreeLinkedList();
+        Node N1 = new Node();
+        N1.value = "N1";
+        Node N2 = new Node();
+        N2.value = "N2";
+        Node N3 = new Node();
+        N3.value = "N3";
+        Node N4 = new Node();
+        N4.value = "N4";
+        Node N5 = new Node();
+        N5.value = "N5";
+        Node N6 = new Node();
+        N6.value = "N6";
+        Node N7 = new Node();
+        N7.value = "N7";
+        Node N8 = new Node();
+        N8.value = "N8";
+        Node N9 = new Node();
+        N9.value = "N9";
+
+        N1.left = N2;
+        N1.right = N3;
+        N2.left = N4;
+        N2.right = N5;
+        N3.left = N6;
+        N3.right = N7;
+        N4.left = N8;
+        N4.right = N9;
+
+        binaryTree.root = N1;
+        binaryTree.preOrder(binaryTree.root);
     }
 }
