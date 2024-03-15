a20b55089f292ee373863251a8336653fa0468de
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-03-15 21:40:47 +0300

    exploring array list

---
+++ src/arrayList/Methods.java
@@ -0,0 +1,13 @@
+package arrayList;
+
+import java.util.*;
+
+public class Methods {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(9, 5, 3, 6, 0, 2, 1);
+        System.out.println(list);
+        Collections.sort(list);
+        System.out.println(list);
+
+    }
+}
