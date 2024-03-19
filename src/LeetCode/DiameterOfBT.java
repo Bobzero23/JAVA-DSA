3ab2316286d027cfd59ec04ca61ca9d0b68155f6
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-03-19 22:43:25 +0300

    diameter of BT implementation complete

--- src/LeetCode/DiameterOfBT.java
+++ src/LeetCode/DiameterOfBT.java
@@ -1,5 +1,9 @@
 package LeetCode;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
 class Node {
     Node left;
     Node right;
@@ -16,9 +20,39 @@ public class DiameterOfBT {
         this.root = null;
     }
 
-    public static int diameterOfBT (Node node) {
+    public static int diameterOfBT (Node root) {
+        Map<Node, Integer> map = new HashMap<>();
+        Stack<Node> stack = new Stack<>();
+        int diameter = 0;
 
+        if (root != null)
+            stack.push(root);
+
+        while (!stack.isEmpty()) {
+            Node node = stack.peek();
+
+            // Fill up stack to perform post-order traversal
+            if (node.left != null && !map.containsKey(node.left)) {
+                stack.push(node.left);
+            } else if (node.right != null && !map.containsKey(node.right)) {
+                stack.push(node.right);
+            } else {
+
+                // Process the root, once left and right subtree have been processed
+                stack.pop();
+                int leftDepth = map.getOrDefault(node.left, 0);
+                int rightDepth = map.getOrDefault(node.right, 0);
+
+                // Put the max depth at a node in the map
+                map.put(node, 1 + Math.max(leftDepth, rightDepth));
+
+                // Update the max diameter found so far
+                diameter = Math.max(diameter, leftDepth + rightDepth);
+            }
+        }
+        return diameter;
     }
+
     public static void main(String[] args) {
         Node N1 = new Node();
         N1.value = 1;
