0f2642445dfbb295be02f78dc42f2efbc4243f98
Bobzero23 <rajabu.mustafa.00@gmail.com>
2024-10-11 10:52:26 +0300

    revising product of array expect self leetcode problem

--- src/LeetCode/ProductOfArrayExceptSelf_238.java
+++ src/LeetCode/ProductOfArrayExceptSelf_238.java
@@ -4,28 +4,29 @@ import java.util.Arrays;
 
 public class ProductOfArrayExceptSelf_238 {
     public static int[] productExceptSelf(int[] nums) {
-        int n = nums.length;
-        int[] output = new int[n];
+        int numsLength = nums.length;
+        int pointer = 0;
+        int[] result = new int[numsLength];
 
-        // Calculate left products
-        int leftProduct = 1;
-        for (int i = 0; i < n; i++) {
-            output[i] += leftProduct;
-            leftProduct *= nums[i];
+        while (pointer < numsLength) {
+            int currentProduct = 1;
+
+            for (int i = 0; i < numsLength; i++) {
+                if (i == pointer) {
+                    continue;
+                }
+                currentProduct = currentProduct * nums[i];
+            }
+
+            result[pointer] = currentProduct;
+            pointer++;
         }
 
-        // Calculate right products and combine with left products
-        int rightProduct = 1;
-        for (int i = n - 1; i >= 0; i--) {
-            output[i] *= rightProduct;
-            rightProduct *= nums[i];
-        }
-
-        return output;
+        return result;
     }
 
     public static void main(String[] args) {
-        int[] nums = {1,2,3,4};
+        int[] nums = {-1,1,0,-3,3};
 
         int[] result = productExceptSelf(nums);
 
